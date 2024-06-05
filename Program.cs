549179c42b0fb671f1f2edbd72e8e3e1c2664b7c
InnetaSh <inneta3@gmail.com>
2024-06-05 17:59:57 +0300

    fix task1,2,3

--- Program.cs
+++ Program.cs
@@ -4,82 +4,104 @@ using System.Net.Mail;
 using System.Threading.Channels;
 using System.Xml.Linq;
 
-//////Бульбашкова сортировка
+////Бульбашкова сортировка
 
-////int[] mas = { 2, 5, 100, -100, 0, 81 };
+//int[] mas = { 2, 5, 100, -100, 0, 81 };
 
-//const int m = 8;
-//int[] mas = new int[m];
-//int l = mas.GetUpperBound(0);
+const int m = 8;
+int[] mas = new int[m];
+int l = mas.GetUpperBound(0);
 
-//for (int i = 0; i <= l; i++)
-//{
-//    Console.Write($"Mas{i}=");
-//    mas[i] = Convert.ToInt32(Console.ReadLine());
-//}
+for (int i = 0; i <= l; i++)
+{
+    Console.Write($"Mas{i}=");
+    while (!Int32.TryParse(Console.ReadLine(), out mas[i]))
+    {
+        Console.WriteLine("Введено не число. Введите число:");
+    }
+}
 
-//for (int i = 0; i <= l; i++)
-//{
-//    Console.Write(mas[i]);
-//    Console.Write("\t");
-//}
-//Console.WriteLine();
+for (int i = 0; i <= l; i++)
+{
+    Console.Write(mas[i]);
+    Console.Write("\t");
+}
+Console.WriteLine();
 
-//for (int i = 0; i <= l; i++)
-//{
-//    for (int j = l; j > i; j--)
-//    {
-//        if (mas[j] < mas[j - 1])
-//        {
-//            int tmp = mas[j];
-//            mas[j] = mas[j - 1];
-//            mas[j - 1] = tmp;
-//        }
+for (int i = 0; i <= l; i++)
+{
+    for (int j = l; j > i; j--)
+    {
+        if (mas[j] < mas[j - 1])
+        {
+            int tmp = mas[j];
+            mas[j] = mas[j - 1];
+            mas[j - 1] = tmp;
+        }
 
-//    }
-//    Console.Write(mas[i]);
-//    Console.Write("\t");
-//}
+    }
+    Console.Write(mas[i]);
+    Console.Write("\t");
+}
+Console.WriteLine();
 
-////Даны цифры двух двузначных чисел, записываемых в виде a2a1 и b2b1 , где a1 и b1 — число единиц, a2 и b2 — число десятков. 
-////    Получить цифры числа, равного разности заданных чисел (известно, что это число двузначное). Число-уменьшаемое, число-вычитаемое и число-разность не определять.
+//Даны цифры двух двузначных чисел, записываемых в виде a2a1 и b2b1 , где a1 и b1 — число единиц, a2 и b2 — число десятков. 
+//    Получить цифры числа, равного разности заданных чисел (известно, что это число двузначное). Число-уменьшаемое, число-вычитаемое и число-разность не определять.
 
-//Console.WriteLine("Enter the digits of a two-digit number1(a2a1)");
-//string num1 = Console.ReadLine();
-//int a2 = Convert.ToInt32(num1[0].ToString());
-//int a1 = Convert.ToInt32(num1[1].ToString());
+Console.WriteLine("Введите двузначное число 1 (a2a1)");
+string num1 = Console.ReadLine(); 
+while (num1.Length != 2)
+{
+    Console.WriteLine("Введено не двухзначное число. Введите двузначное число 1(a2a1)");
+    num1 = Console.ReadLine();
+}
+int a2 = Convert.ToInt32(num1[0].ToString());
+int a1 = Convert.ToInt32(num1[1].ToString());
 
-//Console.WriteLine("Enter the digits of a two-digit number1(a2a1)");
-//string num2 = Console.ReadLine();
-//int b2 = Convert.ToInt32(num2[0].ToString());
-//int b1 = Convert.ToInt32(num2[1].ToString());
+Console.WriteLine("Введите двузначное число 2 (b2b1)");
+string num2 = Console.ReadLine();
+while (num2.Length !=2)
+{
+    Console.WriteLine("Введено не двухзначное число. Введите двузначное 2 число (b2b1)");
+    num2 = Console.ReadLine();
+}
+int b2 = Convert.ToInt32(num2[0].ToString());
+int b1 = Convert.ToInt32(num2[1].ToString());
 
-//int difference = ((a2 * 10 + a1) - (b2 * 10 + b1));
-//Console.Write($"Difference: {num1} - {num2} = {difference}\n");
-//int Difference_a2 = difference / 10;
-//int Difference_a1 = difference % 10;
-//Console.WriteLine($"a2 = {Difference_a2}");
-//Console.WriteLine($"a1 = {Difference_a1}");
+int difference = ((a2 * 10 + a1) - (b2 * 10 + b1));
+Console.Write($"Разность: {num1} - {num2} = {difference}\n");
+int Difference_a2 = difference / 10;
+int Difference_a1 = difference % 10;
+Console.WriteLine($"число десятков = {Difference_a2}");
+Console.WriteLine($"число единиц = {Difference_a1}");
 
 
-////Даны цифры двух десятичных целых чисел: трехзначного a3a2a1 и двузначного b2b1, где a1 и b1 — число единиц, a2 и b2 — число десятков,
-////    a3 — число сотен. Получить цифры, составляющие разность этих чисел (известно, что это число трехзначное). Число-уменьшаемое,
-////    число-вычитаемое и число-разность не определять.
+//Даны цифры двух десятичных целых чисел: трехзначного a3a2a1 и двузначного b2b1, где a1 и b1 — число единиц, a2 и b2 — число десятков,
+//    a3 — число сотен. Получить цифры, составляющие разность этих чисел (известно, что это число трехзначное). Число-уменьшаемое,
+//    число-вычитаемое и число-разность не определять.
 
 
-//Console.WriteLine("Enter the digits of a three-digit number1(a3a2a1)");
-//int num2_1 = Convert.ToInt32(Console.ReadLine());
-//Console.WriteLine("Enter the digits of a two-digit number2(b2b1)");
-//int num2_2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите трехзначное число 1 (a3a2a1)");
+int num2_1;
+while(!Int32.TryParse(Console.ReadLine(), out num2_1) || (num2_1 < 99 || num2_1 > 999))
+{
+    Console.WriteLine("Введено не трехзначное число. Введите трехзначное число 1 (a3a2a1)");
+}
 
-//int difference2 = (num2_1 - num2_2);
-//Console.Write($"Difference2: {num2_1} - {num2_2} = {difference2}\n");
-//int Difference2_a3 = difference2 / 100;
-//int Difference2_a2 = difference2 / 10 % 10;
-//int Difference2_a1 = difference2 % 10;
-//Console.WriteLine($"a3 = {Difference2_a3}");
-//Console.WriteLine($"a2 = {Difference2_a2}");
-//Console.WriteLine($"a1 = {Difference2_a1}");
+Console.WriteLine("Введите двузначное число 2 (b2b1)");
+int num2_2;
+while (!Int32.TryParse(Console.ReadLine(), out num2_2) || (num2_2 < 9 || num2_2 > 99))
+{
+    Console.WriteLine("Введено не двухзначное число. Введите двузначное число 2 (b2b1)");
+}
+int difference2 = (num2_1 - num2_2);
+Console.Write($"Разность2: {num2_1} - {num2_2} = {difference2}\n");
+int Difference2_a3 = difference2 / 100;
+int Difference2_a2 = difference2 / 10 % 10;
+int Difference2_a1 = difference2 % 10;
+Console.WriteLine($"число сотен = {Difference2_a3}");
+Console.WriteLine($"число десятков = {Difference2_a2}");
+Console.WriteLine($"число единиц = {Difference2_a1}");
 
 
 
@@ -89,12 +111,12 @@ using System.Xml.Linq;
 Console.WriteLine("Введите координаты (a, b):");
 int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
-while (a > 8 || a < 0)
+while (a > 8 || a < 1)
 {
     Console.WriteLine("Вышли за границы диапазона. Введите координаты (a):");
     a = Convert.ToInt32(Console.ReadLine());
 }
-while (b > 8 || b < 0)
+while (b > 8 || b < 1)
 {
     Console.WriteLine("Вышли за границы диапазона. Введите координаты (b):");
     b = Convert.ToInt32(Console.ReadLine());
@@ -102,12 +124,12 @@ while (b > 8 || b < 0)
 Console.WriteLine("Введите координаты (c, d):");
 int c = Convert.ToInt32(Console.ReadLine());
 int d = Convert.ToInt32(Console.ReadLine());
-while (c > 8 || c < 0)
+while (c > 8 || c < 1)
 {
     Console.WriteLine("Вышли за границы диапазона. Введите координаты (c):");
     c = Convert.ToInt32(Console.ReadLine());
 }
-while (d > 8 || d < 0)
+while (d > 8 || d < 1)
 {
     Console.WriteLine("Вышли за границы диапазона. Введите координаты (d):");
     d = Convert.ToInt32(Console.ReadLine());
